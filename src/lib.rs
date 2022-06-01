55c48faa8c6b2c4735731333a7001607c3909286
tami5 <kkharji@protonmail.com>
2022-06-01 23:11:34 +0300

    chore: move README to root

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@
 #![deny(nonstandard_style)]
 #![deny(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
-#![doc = include_str!("README.md")]
+#![doc = include_str!("../README.md")]
 
 mod parser;
