1000071bf6322ad12be23449946558e7524a0ae5
Manfred Endres <2523575+Larusso@users.noreply.github.com>
2024-05-10 16:17:35 +0200

    refactor: add ident rule to allow ident starting with underscore (#8)
    I tested this crate with an xcode project created by Unity and it crashed.
    The reason is that unity prefixes some files with `__`
    
    e.g.
    
    `48F04CAAAB193DE466850215 /* __Generated.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; name = __Generated.cpp; path = Classes/Native/__Generated.cpp; sourceTree = SOURCE_ROOT; };`
    
    The project loads and works fine in Xcode so I assume that the error must be in the crate.
    
    I adjusted the grammar and provided another demo file which breaks without the adjusted grammar.

--- src/pbxproj/pest/mod.rs
+++ src/pbxproj/pest/mod.rs
@@ -161,5 +161,5 @@ mod parse_tests {
         };
     }
 
-    test_samples![demo1, demo2, demo3, demo4, demo5, demo6, demo7, demo8, demo9, demo10, demo11];
+    test_samples![demo1, demo2, demo3, demo4, demo5, demo6, demo7, demo8, demo9, demo10, demo11, demo12];
 }
